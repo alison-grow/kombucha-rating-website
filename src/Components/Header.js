cad8f5dd4c2bb06054867ddd62c698c51de9efa5
alison-grow <agrowz98@gmail.com>
2023-01-15 21:24:19 -0800

    continued editing

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,9 +3,10 @@ import React from "react";
 export default function Header() {
     return (
         <div className="header">
-            {/* <img src="https://dl.glitter-graphics.com/pub/256/256843f65gkvdlzq.jpg" alt="mushroom" className="header-img" /> */}
             <h1 className="header-h1">
+                &#9774;
                 My Kombucha Reviews
+                &#9774;
             </h1>
         </div>
     )
